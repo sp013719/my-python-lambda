0a547c0d727864e5bbdb9b88fccc3bf3507cf8a2
Otis Lin <sp013719@gmail.com>
2019-07-24 22:37:15 +0800

    init commit

---
+++ create.py
@@ -0,0 +1,17 @@
+import json
+
+
+def handler(event, context):
+    print('received event{}'.format(event))
+
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
